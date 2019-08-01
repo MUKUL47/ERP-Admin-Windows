30edeeeb6b13c7abdb60cd23c06ef6fdb6a513d0
mukul47 <mukuldut1234@gmail.com>
2019-08-01 16:20:15 +0530

    refactored

--- Database/arrangements.js
+++ Database/arrangements.js
@@ -5,81 +5,76 @@ const admin = require('firebase-admin')
  * Arrangements => arrangementPage, getarrangement, deleteArrangement
  */
 
-module.exports.arrangements = (req,res)=>{
-  firebaseShortcut.dB("StudentLogs").then((s)=>{ 
-    firebaseShortcut.p(s).then((years)=>{
-      res.render("arrangement.ejs",{years : years, arrangements : undefined, yearSelected : undefined})
-    }) 
-})
-//  (async function(){
-//   try{
-//     let s = await firebaseShortcut.dB(),
-//     years = await firebaseShortcut.p(s)
-    
-//   }catch(err){
-//     res.render("arrangement.ejs",{years : years, arrangements : undefined, yearSelected : "some error has occured please refresh the tab"})    
-//   }
-//  })() 
-  }
-  
-  module.exports.getArrangement = (req,res,next)=>{ 
-    (async function f(){
-      try{
-        let getArrangementFiles = await firebaseShortcut.dB(req.query.years+"/seatingArrangement"),
-          files                 = await firebaseShortcut.p(getArrangementFiles),
-          s                     = await firebaseShortcut.dB(),
-          years                 = await firebaseShortcut.p(s);
-      res.render("arrangement.ejs",{years : years, arrangements : files, yearSelected : req.query.years})
-      } 
-      catch(err){
-      res.render("arrangement.ejs",{years : [], arrangements : "", yearSelected : "some error has occured please refresh the tab"})          
-      }      
-    })()
-    /*
-    firebaseShortcut.dB(req.query.years+"/seatingArrangement").
-        then((s)=>{ 
-        firebaseShortcut.p(s).then((files)=>{
-        firebaseShortcut.dB().then((s)=>{
-            firebaseShortcut.p(s).then((years)=>{
-              res.render("arrangement.ejs",{years : years, arrangements : files, yearSelected : req.query.years}) 
-            })})})}) 
-            */  
-  }
-  
-  module.exports.deleteArrangement = (req, res)=>{
-    (async function (){
-      let files = req.body.arrangementFile
-      let year  = req.params.year
-      try{
-        await Promise.all([
-          firebaseShortcut.deleteDbReference(year+"/seatingArrangement/"+files)
-          ,firebaseShortcut.deleteDirectory(year+"/seatingArrangement/"+files+".pdf")
-        ])        
-        let  s                     = await firebaseShortcut.dB(),
-             years                 = await firebaseShortcut.p(s);
-             res.render("arrangement.ejs",{years : years,
-              arrangements : [],
-              yearSelected : "/"+year+"/seatingArrangement/"+R[1]+".pdf deleted"})
-      }
-      catch(err){
-        let  s                     = await firebaseShortcut.dB(),
-             years                 = await firebaseShortcut.p(s);
-             res.render("arrangement.ejs",{years : years, arrangements : "", yearSelected : "some error has occured please try again! : "+err})
-         
-      } 
-    })()
+async function getYears() {
+  let s = await firebaseShortcut.dB(`StudentLogs`),
+    years = await firebaseShortcut.p(s);
+  return years
+}
 
-    /*
-    firebaseShortcut.deleteDirectory(year+"/seatingArrangement/"+files+".pdf")
-    //firebaseShortcut.deleteDbReference(year+"/seatingArrangement/"+files) 
-    firebase.database().ref(year+"/seatingArrangement/"+files).remove()
-    .then(()=>{
-      firebaseShortcut.dB().then((s)=>{
-        firebaseShortcut.p(s).then((years)=>{
-            res.render("arrangement.ejs",{years : years,
-              arrangements : [],
-              yearSelected : "/"+year+"/seatingArrangement/"+files+".pdf deleted"})})})
+async function getArrangementFiles(year) {
+  let getArrangementFiles = await firebaseShortcut.dB(`StudentLogs/${year}/seatingArrangement`),
+    files = await firebaseShortcut.p(getArrangementFiles);
+  return files
+}
+
+module.exports.arrangements = (req, res) => {
+  (async function () {
+    let y;
+    await getYears().then(years => y = years)
+    res.render("arrangement.ejs", {
+      years: y,
+      arrangements: undefined,
+      yearSelected: undefined
     })
-    */
-    
-  }
+  }())
+}
+
+module.exports.getArrangement = (req, res, next) => {
+  (async function () {
+    let year = req.query.years
+    try {
+      let years, files;
+      await getArrangementFiles(year).then(f => files = f)
+      await getYears().then(y => years = y)
+      res.render("arrangement.ejs", {
+        years: years,
+        arrangements: files,
+        yearSelected: req.query.years
+      })
+    } catch (err) {
+      res.render("arrangement.ejs", {
+        years: [],
+        arrangements: "",
+        yearSelected: "some error has occured please refresh the tab"
+      })
+    }
+  })()
+}
+
+module.exports.deleteArrangement = (req, res) => {
+  (async function () {
+    let file = req.body.arrangementFile,
+      year = req.params.year,
+      years;
+    try {
+      await Promise.all([
+        firebaseShortcut.deleteDbReference(`StudentLogs/${year}/seatingArrangement/${file}`),
+        firebaseShortcut.deleteDirectory(`${year}/seatingArrangement/${file}.pdf`)
+      ])
+      await getYears().then(year => years = year);
+      res.render("arrangement.ejs", {
+        years: years,
+        arrangements: [],
+        yearSelected: "/" + year + "/seatingArrangement/" + file + ".pdf deleted"
+      })
+    } catch (err) {
+      await getYears().then(year => years = year);
+      res.render("arrangement.ejs", {
+        years: years,
+        arrangements: [],
+        yearSelected: "some error has occured please try again! : " + err
+      })
+    }
+  })()
+
+}
--- Database/home.js
+++ Database/home.js
@@ -5,78 +5,122 @@ const admin = require('firebase-admin')
  * searchPass, updateAdmin, DeleteUser, update/AddUser, deleteYearDirectory,
  * deleteBatchDirectory, deleteSemesterDirectory
  */
-module.exports.getUserPass = (req,res,next)=>{
-    T = undefined
-    firebaseShortcut.dB().then((s)=>{
-      s.forEach((i)=>{
-      var passId = i.val(); 
-      for (var i = 0, keys = Object.keys(passId), ii = keys.length; i < ii; i++) {
-          if(keys[i].toUpperCase() === req.query.id.toUpperCase()){     
-            T = keys[i].toUpperCase()+" : "+passId[keys[i]]    
-          }};})
-}).then(()=>{
-  module.exports.T = T
-  next() 
-})
-};
-
-    module.exports.updateAdmin = (req, res, next)=>{
-     firebaseShortcut.setDbReference("ADMIN/ADMIN","Updated at : "+new Date().toString().substring(0,25)+"\n"+req.query.admin)
-    .then(()=>{
-         res.render("index.ejs",{userPass : "Admin : "+req.query.admin })
-        })
-  }
-
-  module.exports.deleteUser = (req,res,next)=>{
-    let year = req.query.id
-    firebaseShortcut.deleteDbReference(year.substring(0,4)+"/"+year.toUpperCase())
-    .then(
-      res.render("index.ejs",{userPass : year+" deleted"})
-    ).catch(
-      res.render("index.ejs",{userPass : "Error"})
-    )
-  }
-
-  module.exports.deleteYearId = (req,res,next)=>{    
-    (async function(){
-      let year = req.query.year
-      try{
-        await firebaseShortcut.deleteDirectory(year)
-        await firebaseShortcut.deleteDbReference(year)      
-        res.render("index.ejs",{userPass : "Deleted"})
-      }
-      catch(err){
-        res.render("index.ejs",{userPass : "Error"})
-      } 
-    }())   
-  }
-  
-  module.exports.updateRecord = (req,res,next)=>{
-    firebaseShortcut.setDbReference("StudentLogs/"+
-        (req.query.id).substring(0,4)+"/"+(req.query.id).toUpperCase(),req.query.pass)
-    .then((err)=>{
-      res.render("index.ejs",{userPass : req.query.id +" updated"})
+module.exports.getUserPass = (req, res, next) => {
+  let id = req.query.id.toUpperCase().trim()
+  if (id.length < 4) {
+    res.render("index.ejs", {
+      userPass: "Invalid Id"
     })
+  } else {
+    firebaseShortcut.dB(`StudentLogs/${id.substring(0,4)}`)
+      .then(msg => {
+        let pass = "ID NOT FOUND"
+        msg.forEach(kV => {
+          if (kV.key.toUpperCase() === id) pass = kV.val()
+        })
+        res.render("index.ejs", {
+          userPass: `${id} : ${pass}`
+        })
+      })
+      .catch(err => res.render("index.ejs", {
+        userPass: err
+      }))
   }
 
-  module.exports.deleteYear = (req,res)=>{ 
-    firebaseShortcut.deleteDbReference(
-        req.body.year+"/seatingArrangement").then(
-      ()=>{
-        firebaseShortcut.deleteDirectory(req.body.year).then(
-          res.render("index.ejs",{userPass : "directory : /"+req.body.year+" deleted!"}) )
-      })  
-  }
-  
-  module.exports.deleteBatch = (req,res)=>{
-    firebaseShortcut.deleteDirectory(req.body.year+"/"+req.body.batch).then(
-      res.render("index.ejs",{userPass : "directory : /"+req.body.year+"/"+req.body.batch+" deleted!"}) 
-    )
-  }
-  
-  module.exports.deleteSem = (req,res)=>{
-    firebaseShortcut.deleteDirectory(req.body.year+"/"+req.body.batch+"/"+req.body.sem)
+
+}
+module.exports.updateAdmin = (req, res, next) => {
+  firebaseShortcut.setDbReference("ADMIN/ADMIN", "Updated at : " + new Date().toString().substring(0, 25) + "\n" + req.query.admin)
+    .then(() => {
+      res.render("index.ejs", {
+        userPass: "Admin : " + req.query.admin
+      })
+    })
+    .catch(err => res.render("index.ejs", {
+      userPass: err
+    }))
+}
+
+module.exports.deleteUser = (req, res, next) => {
+  let year = req.query.id.toUpperCase()
+  firebaseShortcut.deleteDbReference(`StudentLogs/${year.substring(0,4)}/${year}`)
     .then(
-      res.render("index.ejs",{userPass : "directory : /"+req.body.year+"/"+req.body.batch+"/"+req.body.sem+" deleted!"})  
+      res.render("index.ejs", {
+        userPass: year + " deleted"
+      })
+    ).catch(
+      res.render("index.ejs", {
+        userPass: "User not found"
+      })
     )
+}
+
+module.exports.deleteYearId = (req, res, next) => {
+  (async function () {
+    let year = req.query.year
+    try {
+      await firebaseShortcut.deleteDirectory()
+      await firebaseShortcut.deleteDbReference(`StudentLogs/${year}`)
+      res.render("index.ejs", {
+        userPass: `${year} Deleted`
+      })
+    } catch (err) {
+      res.render("index.ejs", {
+        userPass: `Error : ${err}`
+      })
+    }
+  }())
+}
+
+module.exports.updateRecord = (req, res, next) => {
+  let id = req.query.id.toUpperCase().trim(),
+    pass = req.query.pass.toUpperCase().trim();
+  if (id.length < 5) res.render("index.ejs", {
+    userPass: `Invalid Id : ${id}`
+  })
+  else {
+    firebaseShortcut.setDbReference(`StudentLogs/${id.substring(0,4)}/${id}`, pass)
+      .then((err) => {
+        res.render("index.ejs", {
+          userPass: `${req.query.id.toUpperCase().trim()} updated`
+        })
+      }).catch(err =>
+        res.render("index.ejs", {
+          userPass: `Error : ${err}`
+        })
+      )
   }
+}
+
+module.exports.deleteYear = (req, res) => {
+  let year = req.body.year
+  firebaseShortcut.deleteDbReference(year).then(
+    () => {
+      firebaseShortcut.deleteDirectory(year).then(
+        res.render("index.ejs", {
+          userPass: `Directory : ${year} deleted`
+        }))
+    }).catch(err =>
+    res.render("index.ejs", {
+      userPass: `Error : ${err}`
+    })
+  )
+}
+/*
+module.exports.deleteBatch = (req, res) => {
+  firebaseShortcut.deleteDirectory(req.body.year + "/" + req.body.batch).then(
+    res.render("index.ejs", {
+      userPass: "directory : /" + req.body.year + "/" + req.body.batch + " deleted!"
+    })
+  )
+}
+
+module.exports.deleteSem = (req, res) => {
+  firebaseShortcut.deleteDirectory(req.body.year + "/" + req.body.batch + "/" + req.body.sem)
+    .then(
+      res.render("index.ejs", {
+        userPass: "directory : /" + req.body.year + "/" + req.body.batch + "/" + req.body.sem + " deleted!"
+      })
+    )
+}
+*/
